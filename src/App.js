9c26e9e6ff809c98a33231a323e05a0e89e82dc2
Ehiz-js <azamegbesamuel@gmail.com>
2025-11-08 07:02:35 +0100

    feat: add sky component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Canvas } from '@react-three/fiber';
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return (
-    <>
-      <div>Outside Canvas</div>
-      <Canvas>
-      </Canvas>
-    </>
-  );
+	return (
+		<>
+			<Canvas>
+				<Sky sunPosition={[100, 100, 20]} />
+			</Canvas>
+		</>
+	);
 }
 
 export default App;
